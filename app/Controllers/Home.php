8f30d7516f3437868d90917b6b4a00aecffc4e23
adrianismu <adrianismu263@gmail.com>
2023-11-25 19:15:36 +0700

    Add Files

---
+++ app/Controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function hello(): string
+    {
+        return view('hello');
+    }
+
+    public function ppdb(): string
+    {
+        return view('ppdb');
+    }
+}
---
+++ app/Views/hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+
+<?php
+    echo "<h1>Hello, World! Welcome to Code Igniter 4</h1>";
+?>
+
+</body>
+</html>
